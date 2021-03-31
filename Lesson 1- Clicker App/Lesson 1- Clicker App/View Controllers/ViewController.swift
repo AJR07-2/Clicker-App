0e31d1b7074ac2f918dde34c71834251425733d5
Ang Jun Ray <angjunray@live.com>
2021-03-31 23:16:34 +0800

    Start and stop

--- Lesson 1- Clicker App/Lesson 1- Clicker App/View Controllers/ViewController.swift
+++ Lesson 1- Clicker App/Lesson 1- Clicker App/View Controllers/ViewController.swift
@@ -7,22 +7,28 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseFirestore
 
 class ViewController: UIViewController {
 
-    
+    var timer = Timer()
+    var timeElapsed = 0.0
     @IBOutlet weak var Settings: UIButton!
+    @IBOutlet weak var LeaderBoard: UIButton!
     @IBOutlet weak var congratsLabel: UILabel!
     @IBOutlet weak var clickButton: UIButton!
     @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var timerLabel: UILabel!
     
     var counter = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
         let image = UIImage(named: "settingsIcon")
-        Settings.frame = CGRect(x: 30, y: 40, width: 20, height: 20)
         Settings.setBackgroundImage(image, for: UIControl.State.normal)
+        
+        let image2 = UIImage(named: "trophyIcon")
+        LeaderBoard.setBackgroundImage(image2, for: UIControl.State.normal)
 
         clickButton.layer.cornerRadius = 10
         congratsLabel.isHidden = true;
@@ -32,6 +38,8 @@ class ViewController: UIViewController {
         counter+=1
         if(counter == 1){
             clickButton.backgroundColor = .red
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+            timerLabel.isHidden = false
         }
         clickButton.setTitle("\(counter)", for: .normal)
         if(counter > 20 && counter < 40){
@@ -42,7 +50,72 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func goToSettings(_ sender: Any) {
+    @objc func updateCounter(){
+        timeElapsed += 0.1
+        timerLabel.text = "Elapsed Time: \(Double(round(10*timeElapsed)/10))s"
+        if(Double(round(10*timeElapsed)/10) == 60.0){
+            print("Tmeeee's up!")
+            timerLabel.text = "TIME'S UP!"
+            timer = Timer()
+            timeElapsed = 0.0
+            if(FirebaseAuth.Auth.auth().currentUser != nil){
+                if(counter > 20){
+                    let database = FirebaseFirestore.Firestore.firestore()
+
+                    let date = Date()
+                    let calendar = Calendar.current
+                    let year = calendar.component(.year, from: date)
+                    let month = calendar.component(.month, from: date)
+                    let day = calendar.component(.day, from: date)
+                    let hour = calendar.component(.hour, from: date)
+                    let minutes = calendar.component(.minute, from: date)
+                    
+                    database.collection("User").document(FirebaseAuth.Auth.auth().currentUser!.email!).collection("History").document().setData([
+                        "time": [year, month, day, hour, minutes],
+                        "score": counter,
+                    ])
+                    
+                    database.collection("User").document(FirebaseAuth.Auth.auth().currentUser!.email!)
+                    
+                    let alert  = UIAlertController(title: "Added to user history!", message: "Your score is \(counter)", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Cool", style: .cancel, handler: {_ in
+                        
+                    }))
+                    present(alert, animated: true)
+                }else{
+                    let alert  = UIAlertController(title: "It seems you are afk, your counts are very low", message: "It shall not be added to history", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: {_ in
+                        
+                    }))
+                    present(alert, animated: true)
+                }
+                
+            }else{
+                let alert  = UIAlertController(title: "Could not add to user history, you are not signed in", message: "Your score is \(counter), to keep track of your clicks, please sign in in the future", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: {_ in
+                    
+                }))
+                present(alert, animated: true)
+            }
+            
+            counter = 0
+            clickButton.setTitle("Start", for: .normal)
+            clickButton.backgroundColor = .yellow
+        }
+    }
+    
+    @IBAction func goToLeaderBoards(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        if let third = storyboard.instantiateViewController(withIdentifier: "VC3") as? ViewController3{
+            self.present(third, animated: true, completion: nil)
+        }else{
+            print("Something went wrong :(1")
+        }
+    }
+    
+    
+    @IBAction func goToUserSettings(_ sender: Any){
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         
         if let secondVC = storyboard.instantiateViewController(withIdentifier: "VC2") as? ViewController2{
